4e4265f551d38e93887f7c6a3ca24835e8bd1733
abaid778 <abaid.gulshan@gmail.com>
2015-07-29 15:00:12 +0500

    add commands

--- AWS-EC2-Mysql-WebDirectory-Backup/dbbackup.sh
+++ AWS-EC2-Mysql-WebDirectory-Backup/dbbackup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATESTAMP=$(date +%Y-%m-%d)
 PASS="mypassword"
-cd /back/dailybackup/
+mkdir -p /backup/dailybackup/
+cd /backup/dailybackup/
 mysqldump -u root -h DB.rds.amazonaws.com -p$PASS --all-databases | gzip > alldatabases.$DATESTAMP.sql.gz
-find /back/dailybackup/ -type f -mtime +6 -delete
+find /backup/dailybackup/ -type f -mtime +6 -delete
