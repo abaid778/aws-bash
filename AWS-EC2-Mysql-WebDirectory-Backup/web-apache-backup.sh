86283e93434487358001cc1ce53275dcdf7d90ff
Abaid <abaid.gulshan@gmail.com>
2015-08-07 19:36:04 +0500

    script changed

--- AWS-EC2-Mysql-WebDirectory-Backup/web-apache-backup.sh
+++ AWS-EC2-Mysql-WebDirectory-Backup/web-apache-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DATESTAMP=$(date +%Y-%m-%d)
+mkdir -p /backup/dailywebbackup/
 cd /backup/dailywebbackup/
 tar -zcvf "allwebsite.$(date +%m-%d-%y).tar.gz" /var/www/html/
 tar -zcvf "apacheconfig.$(date +%m-%d-%y).tar.gz" /etc/apache2/sites-available/
